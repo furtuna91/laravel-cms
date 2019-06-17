c32afed92f564aef62086a7ad3015b1a00aa15f7
Furtuna Laurentiu <virgil.furtuna@gmail.com>
2019-06-18 00:08:45 +0300

    Show Users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,29 @@
-<h1>its working</h1>
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Users</h1>
+    <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Created</th>
+            <th scope="col">Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+            @if (count($users) > 0)
+                @foreach ($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->created_at->diffForHumans() }}</td>
+                        <td>{{ $user->updated_at->diffForHumans() }}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+      </table>
+@stop
