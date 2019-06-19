28a2fcc73eae9ebd8c9b24008d17b8a96608c835
Furtuna Virgil <virgil.furtuna@gmail.com>
2019-06-19 19:16:56 +0300

    Add the proper header

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Users</h1>
+    <div class="page-header">
+        <div class="row align-items-end">
+            <div class="col-lg-8">
+                <div class="page-header-title">
+                    <i class="ik ik-credit-card bg-blue"></i>
+                    <div class="d-inline">
+                        <h5>Users</h5>
+                        <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <nav class="breadcrumb-container" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="../../index.html"><i class="ik ik-home"></i></a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="#">UI</a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="#">Basic</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">Alerts</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
     <div class="card">
         <div class="card-header row">
             <div class="col col-sm-3">
